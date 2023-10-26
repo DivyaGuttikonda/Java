501d4eb0583376dab625c107ab5c10e664a90628
DivyaGuttikonda <divya.guttikonda@qualizeal.com>
2023-10-26 14:48:02 +0530

    file uploaded

---
+++ conditionalstatements/Main.java
@@ -0,0 +1,15 @@
+package conditionalstatements;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        int Age =  20;
+        int Vage = 18;
+
+        if (Age >= Vage) {
+            System.out.println("Old enough to vote!");
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+    }
+}
