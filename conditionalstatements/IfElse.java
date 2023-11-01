80adb6d5be40f2e2c279d1dea04acb4ba9b7bb41
DivyaGuttikonda <divya.guttikonda@qualizeal.com>
2023-11-01 09:31:52 +0530

    new files added

--- conditionalstatements/IfElse.java
+++ conditionalstatements/IfElse.java
@@ -1,2 +1,15 @@
-package conditionalstatements;public class IfElse {
+package conditionalstatements;
+
+public class IfElse {
+    public static void main (String[] args)
+    {
+        int Age =  20;
+        int age = 18;
+
+        if (Age >= age) {
+            System.out.println("Old enough to vote..");
+        } else {
+            System.out.println("Not old enough to vote..");
+        }
+    }
 }
